8a4348fb7c4f0e31f2478f996bf870db040674eb
OGCiel <piscesmesh14@gmail.com>
2022-11-18 14:52:28 +0700

    1
    PWA

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { Dashboard2Component } from './dashboard2/dashboard2.component';
+import { Dashboard3Component } from './dashboard3/dashboard3.component';
+import { LoginComponent } from './login/login.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { SignupComponent } from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "admin", component: AdminComponent },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "dashboard2", component: Dashboard2Component },
+  { path: "dashboard3", component: Dashboard3Component },
+  { path: "login", component: LoginComponent },
+  { path: "signup", component: SignupComponent },
+  { path: "mahasiswa", component: MahasiswaComponent },
+  { path: "", redirectTo: "login", pathMatch: "full" }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/dashboard3/dashboard3.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Renderer2 } from '@angular/core';
+
+declare const $ : any;
+
+@Component({
+  selector: 'app-dashboard3',
+  templateUrl: './dashboard3.component.html',
+  styleUrls: ['./dashboard3.component.css']
+})
+export class Dashboard3Component implements OnInit {
+
+  constructor(private renderer : Renderer2) { }
+
+  ngAfterViewInit(): void{
+    this.renderer.removeClass(document.body, "login-page");
+    this.renderer.removeClass(document.body, "sidebar-open");
+    this.renderer.addClass(document.body, "sidebar-closed");
+
+    //$('[data-widget="treeview"]').Treeview("init"); //to enable the side-menu item drip down
+  }
+
+  ngOnInit(): void {
+  }
+
+}
